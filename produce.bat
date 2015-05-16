6d43ea643e2008e597a6d9cfc08bbd2aca64c9cd
suzutan <wuyudeyang@gmail.com>
2015-05-16 20:05:10 +0800

    Signed-off-by: suzutan <wuyudeyang@gmail.com>

--- produce.bat
+++ produce.bat
@@ -1,2 +1,2 @@
-gfwlist2pac -i %~dp0\gfwlist.txt -f %~dp0\test\proxy_abp.pac -p "PROXY thenorth.f.ftq.me:17734" --precise
-gfwlist2pac -i %~dp0\gfwlist.txt -f %~dp0\test\proxy.pac -p "PROXY 61.183.128.2:17734"
+gfwlist2pac -i %~dp0\gfwlist.txt -f %~dp0\test\proxy_abp.pac -p "PROXY 61.183.128.2:4004" --precise
+gfwlist2pac -i %~dp0\gfwlist.txt -f %~dp0\test\proxy.pac -p "PROXY 61.183.128.2:4004"
